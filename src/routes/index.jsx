8fe92cf41fe636f4e746d206c0d0ecdef2dadda2
Rachid Echafai <rachidchaf2001@gmail.com>
2025-02-06 11:52:28 +0100

    initialize project with React, Vite, and essential configurations

---
+++ src/routes/index.jsx
@@ -0,0 +1,20 @@
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Loader from '../components/common/Loader';
+
+const Dashboard = lazy(() => import('../pages/backOffice'));
+const Reviews = lazy(() => import('../pages/backOffice/reviews'));
+
+function AppRouter() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="dashboard" index element={<Dashboard />} />
+        <Route path="reviews" element={<Reviews />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </Suspense>
+  );
+}
+
+export default AppRouter;
